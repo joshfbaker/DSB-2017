6c3194fe542556c68c222da0b2b1da3522d7b0e4
Josh Baker <joshfbaker@gmail.com>
2017-03-26 14:00:59 -0400

    Commenting Code

--- kaggletutorialcode/LUNA_train_unet.py
+++ kaggletutorialcode/LUNA_train_unet.py
@@ -5,6 +5,8 @@ Created on Sat Feb 25 11:37:42 2017
 @author: 572203
 """
 
+# JFB: This code is pretty poorly commented.  Adding additional comments
+
 from __future__ import print_function
 
 import numpy as np
@@ -15,8 +17,14 @@ from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
 
+# Any way to pull out paths into a separate config file?  Add this to the Git Ignore file?
 working_path = "C:/Users/572203/Documents/Data Science Bowl 2017/LUNA16/tutorial/"
 
+# Theano?! I thought they were using TensorFlow backend?
+# Could be that they are using the Theano dimension ordering (z, x, y)
+# rather than TensorFlow dimension ordering (x, y, z) but still using 
+# TensorFlow backend.  If this is the case, the code should be refactored
+# to avoid future confusion
 K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
 img_rows = 512
@@ -24,25 +32,45 @@ img_cols = 512
 
 smooth = 1.
 
-
 def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+#Why do we have effectively the same function here twice?
 def dice_coef_np(y_true,y_pred):
     y_true_f = y_true.flatten()
     y_pred_f = y_pred.flatten()
     intersection = np.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
+# Does this really need to be a function?  Just a simple negation correct?
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+# Ahh as dice_coef and dice_coef_loss are both being fed to the kera.model funtion
+# as arguments, they probably do need to be their own functions
+
+
+
+# get_unet builds the conv net architecture.  Note the overall pattern:
+# - 4x conv-conv-pool
+# - 1x conv-conv
+# - 4x up-conv-conv
+
+# Activation function at all layers is ReLU except for final layer
+
+# Note how the num_filters increases 32 > 64 > 128 > 256 > 512 > 256 > 128 > 64 > 32
+# This progression is likely the source of the name "u-net"
 
 def get_unet():
     inputs = Input((1,img_rows, img_cols))
+
+    # Note that inputs is not an argument to a function, but rather being multiplied
+    # by the output of Convolution2D...
+
+    # Syntax is Convolution2D(num_filters, num_rows_per_kernel, num_cols_per_kernel)
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
@@ -78,8 +106,10 @@ def get_unet():
     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
 
+    # This is the output layer
     conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
 
+    # Simply pass the input and output layer to the model function
     model = Model(input=inputs, output=conv10)
 
     model.compile(optimizer=Adam(lr=1.0e-5), loss=dice_coef_loss, metrics=[dice_coef])
@@ -115,17 +145,23 @@ def train_and_predict(use_existing):
     if use_existing:
         model.load_weights('./unet.hdf5')
         
-    # 
+    # Original Author:
     # The final results for this tutorial were produced using a multi-GPU
     # machine using TitanX's.
     # For a home GPU computation benchmark, on my home set up with a GTX970 
     # I was able to run 20 epochs with a training set size of 320 and 
     # batch size of 2 in about an hour. I started getting reseasonable masks 
     # after about 3 hours of training. 
-    #
+    
+    # JFB:
+    # Only used a training set of 320! This is much less than the full training set. 
+    # May acheive better results on full dataset
+
     print('-'*30)
     print('Fitting model...')
     print('-'*30)
+
+    #mode;.fit handles the complexity of batching, cross validating and looping
     model.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=20, verbose=1, shuffle=True,
               callbacks=[model_checkpoint])
 
@@ -138,15 +174,22 @@ def train_and_predict(use_existing):
     print('-'*30)
     print('Predicting masks on test data...')
     print('-'*30)
+    
     num_test = len(imgs_test)
+    # Initialize the numpy ND array
     imgs_mask_test = np.ndarray([num_test,1,512,512],dtype=np.float32)
+    
+    # Predict the nodule mask for every image in our test set
     for i in range(num_test):
         imgs_mask_test[i] = model.predict([imgs_test[i:i+1]], verbose=0)[0]
     np.save('masksTestPredicted.npy', imgs_mask_test)
+    
+    # Calculate the mean error per the dice coefficient
     mean = 0.0
     for i in range(num_test):
         mean+=dice_coef_np(imgs_mask_test_true[i,0], imgs_mask_test[i,0])
     mean/=num_test
+    
     print("Mean Dice Coeff : ",mean)
 
 if __name__ == '__main__':
