b9b7ccd31517cd7c67dde240eab0386bcdb8d22d
penguinskid <penguinskid@users.noreply.github.com>
2017-03-26 12:14:30 -0400

    Update LUNA_mask_extraction.py

--- kaggletutorialcode/LUNA_mask_extraction.py
+++ kaggletutorialcode/LUNA_mask_extraction.py
@@ -126,10 +126,10 @@ for fcount, img_file in enumerate(tqdm(file_list)):
             masks = np.ndarray([int(nodule_max*2+1),height,width],dtype=np.uint8)
             for i, i_z in enumerate(np.arange(int(v_center[2])-nodule_max,
                              int(v_center[2])+(nodule_max+1)).clip(0, num_z-1)): # clip prevents going out of bounds in Z
-                try:
-                    slice_diam = np.sqrt((diam/2)**2-(np.abs(i_z-v_center[2])*spacing[2])**2)*2
-                except: slice_diam = 0
-                mask = make_mask(center, slice_diam, i_z*spacing[2]+origin[2],
+                #try:
+                    #slice_diam = np.sqrt((diam/2)**2-(np.abs(i_z-v_center[2])*spacing[2])**2)*2
+                #except: slice_diam = 0
+                mask = make_mask(center, diam, i_z*spacing[2]+origin[2],
                                  width, height, spacing, origin)
                 masks[i] = mask
                 imgs[i] = img_array[i_z]
